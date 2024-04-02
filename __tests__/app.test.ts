d644223bf5f6b37774e2edd05abf32206dded9a1
Thiago Domingues <thenrique2012@gmail.com>
2024-04-01 23:27:36 -0300

    chore: first commit

---
+++ __tests__/app.test.ts
@@ -0,0 +1,10 @@
+import { GildedRose } from '../src/app';
+import { Item } from '../src/Item';
+
+describe('Gilded Rose', () => {
+  it('should foo', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('foo');
+  });
+});
