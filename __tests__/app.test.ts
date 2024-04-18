6486bc15afeb602e1b3a575c25bed99e43a2bf3d
Thiago Domingues <thenrique2012@gmail.com>
2024-04-17 23:27:59 -0300

    test: write app tests

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,10 +1,19 @@
 import { GildedRose } from '../src/app';
+import records from '../out/records.json';
 import { Item } from '../src/Item';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
-  });
+  const entries = Object.entries(records);
+
+  for (let [key, value] of entries) {
+    const [name, sellIn, quality] = key.split(';');
+
+    const gildedRose = new GildedRose([
+      new Item(name, Number(sellIn), Number(quality))
+    ]);
+
+    it(`${[name, sellIn, quality]}`, () => {
+      expect(gildedRose.updateQuality()[0]).toEqual(value);
+    });
+  }
 });
